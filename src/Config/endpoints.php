2ce64de37b14b8b199aa4b886b6ae3b36f0c4c21
Paklic <Paklic71@gmail.com>
2026-03-04 23:04:08 +0100

    New: Added more endpoints

--- src/Config/endpoints.php
+++ src/Config/endpoints.php
@@ -1,14 +1,26 @@
 <?php
-function getEndpoint($user,$tagLine,$location,$puuid,$lookingFor,$pfpID,$matchID,$summonerID){
+function getEndpoint($user,$tagLine,$location,$puuid,$lookingFor,$pfpID,$matchID,$summonerID,$championName,$itemID){
+    $version = "14.24.1";
     switch ($lookingFor) {
         case "puuid":
             return "/riot/account/v1/accounts/by-riot-id/$user/$tagLine";
-            break;
         case "summoner":
             return "/lol/summoner/by-puuid/$puuid";
-            break;
+        case "mastery":
+            return "/lol/champion-mastery/v4/champion-masteries/by-puuid/$puuid";
+        case "pfp":
+            return "https://ddragon.leagueoflegends.com/cdn/14.24.1/img/profileicon/$pfpID";
+        case "matchesID":
+            return "/lol/match/v5/matches/by-puuid/$puuid/ids";
+        case "rankedStats":
+            return "/lol/league/v4/entries/by-puuid/$puuid";
+        case "match":
+            return "/lol/match/v5/matches/$matchID";
+        case "championImage":
+            return "https://ddragon.leagueoflegends.com/cdn/$version/img/champion/$championName.png";
+        case "itemImage":
+            return "https://ddragon.leagueoflegends.com/cdn/$version/img/item/$itemID.png";
         default:
-            # code...
             break;
     }
 }
--- src/Services/RiotApiService.php
+++ src/Services/RiotApiService.php
@@ -31,8 +31,4 @@ class ApiClient{
         ];
     }
 }
-
-
-
-
 ?>
