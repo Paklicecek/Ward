dfb8a1b83307714b616d4416a3ba07c02484cd89
Paklic <Paklic71@gmail.com>
2026-04-04 13:25:36 +0200

    Update: userStats function added to grab user stats from json sent by riot API

--- src/Controllers/StatsController.php
+++ src/Controllers/StatsController.php
@@ -30,22 +30,12 @@ class StatsController
         $summoner = $this->client->getServer(getEndpoint("summoner", puuid: $puuid));
         $rankedStats = $this->client->getServer(getEndpoint("rankedStats", puuid: $puuid));
 
-        $match = $this->client->getServer(getEndpoint("match", matchId: $matchesID));
-        $match = $match["data"]["info"];
-        $matches = [
-            "match1" => [
-                "generalInfo" => [
-                    "gameMode" => $match["gameMode"],
-                    "queueID" => $match["queueID"],
-                    "duration" => $match["gameDuration"],
-                ],
-                "playerTeam" => [
-                ],
-                "enemyTeam" => [
+        foreach ($matchesID as $matchID) {
+            $match = $this->client->getServer(getEndpoint("match", matchId: $matchID));
+            $match = $match["data"];
+            $stats = $this->userStats($puuid, $match);
 
-                ],
-            ]
-        ];
+        }
 
 
         return [
@@ -55,6 +45,31 @@ class StatsController
             "level" => $summoner["data"]["summonerLevel"],
             "rankedStats" => $rankedStats["data"]
         ];
+    }
+
+    private function userStats
+    (
+        $puuid = null,
+        $match = null
+    ): array|null
+    {
+        $participants = $match["info"]["participants"];
+        foreach ($participants as $participant) if ($participant["puuid"] == $puuid) {
+            return [
+                "kills" => $participant["kills"],
+                "deaths" => $participant["deaths"],
+                "assists" => $participant["assists"],
+                "kda" => ($participant["kills"] + $participant["assists"]) / $participant["deaths"],
+                "cs" => $participant["totalMinionsKilled"] + $participant["neutralMinionsKilled"],
+                "vision" => $participant["visionScore"],
+                "champion" => $participant["championId"],
+                "mainSummoner" => $participant["summoner1Id"],
+                "secondSummoner" => $participant["summoner2Id"],
+                "mainRune" => $participant["perks"]["styles"][0]["selections"][0]["perk"],
+                "secondRune" => $participant["perks"]["styles"][1]["style"],
+            ];
+        }
+        return [];
     }
 }
 
