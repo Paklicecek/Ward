69fe6d0b6cb5670169b38e2022fd68e4e5f54886
Paklic <Paklic71@gmail.com>
2026-04-06 14:51:05 +0200

    Update: updated stats loading

--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,11 @@
 const searchInput = document.querySelector(".searchInput")
 const regionSelect = document.querySelector(".regionSelect")
 const button = document.querySelector(".searchBtn")
-const app = document.querySelector(".app")
 
 button.addEventListener("click", () => {
     const region = regionSelect.value
     const [user, tagLine] = searchInput.value.split('#');
-    fetch(`/api.php?user=${user}&tagLine=${tagLine}&region=${region}`)
-        .then(res => res.json())
-        .then(data => {
-            console.log(data)
-            // renderStats(data)
-        })
+    if (user && tagLine != null) {
+        window.location.href = `/stats.html?user=${user}&tagLine=${tagLine}&region=${region}`
+    }
 })
-
-// function renderStats(data) {
-//     app.innerHTML = `<h1>${data.rankedStats[0].tier}</h1>`
-// }
---
+++ public/js/stats.js
@@ -0,0 +1,14 @@
+const params = new URLSearchParams(window.location.search)
+const user = params.get('user')
+const tagLine = params.get('tagLine')
+const region = params.get('region')
+
+fetch(`/api.php?user=${user}&tagLine=${tagLine}&region=${region}`)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        // renderStats(data)
+    })
+    .catch(error => {
+        console.error("Error fetching stats:", error);
+    })
