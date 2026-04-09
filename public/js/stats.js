d273ed63957e8adc2e19165bbf2605aa8069e6da
Paklicecek <Paklic71@gmail.com>
2026-04-09 17:24:23 +0200

    Update: connecting backend data to frontend

--- public/js/stats.js
+++ public/js/stats.js
@@ -7,7 +7,7 @@ fetch(`/api.php?user=${user}&tagLine=${tagLine}&region=${region}`)
     .then(res => res.json())
     .then(data => {
         console.log(data)
-        // renderStats(data)
+        renderStats(data)
     })
     .catch(error => {
         console.error("Error fetching stats:", error)
@@ -19,6 +19,10 @@ function renderStats(data) {
     const taglineEl = document.querySelector('.searchedTagLine')
     const levelEl = document.querySelector('.levelContainer')
     const profileImgEl = document.querySelector('.pfpContainer img')
+    usernameEl.innerHTML = user
+    taglineEl.innerHTML = tagLine
+    levelEl.innerHTML = data.level
+    profileImgEl.src = `https://static.bigbrain.gg/assets/lol/riot_static/16.5.1/img/profileicon/${data.profileIcon}.png`
 
     // === 2. RANKED FLEX CARD ===
     const flexRankImgEl = document.querySelector('.description .imgContainer img')
@@ -27,6 +31,7 @@ function renderStats(data) {
     const flexWinLossEl = document.querySelector('.winAndLosses')
     const flexWinRateEl = document.querySelector('.winRate')
 
+    
     // === 3. MATCH HISTORY HEADER ===
     const recentWinRateDonutEl = document.querySelector('.donut') // Needs style="--win-rate: XX%"
     const recentWinRateTextEl = document.querySelector('.winRateContainer .winRate')
