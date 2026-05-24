cc3b1b92c814bc07f054864612148513d68de753
Paklicecek <Paklic71@gmail.com>
2026-05-24 10:54:51 +0200

    get me out of here pls

--- src/footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="footerContainer">
-  <div class="footerLogo">WARD<span class="logoHighlight">.GG</span></div>
-  <p class="footer-text">Not affiliated with Riot Games · Patch 26.2</p>
-  <p class="footer-text">EUW · NA · KR · EUN · BR</p>
-</footer>
-</body>
-</html>
